1a096ef8bf6a643553fe84ab045c772c7a9ad567
Adharsh S <s.adharsh.97@gmail.com>
2023-04-13 16:00:01 -0400

    code cleanup

--- book/labs/analysis/pedometer/accelerometer_averaged.py
+++ book/labs/analysis/pedometer/accelerometer_averaged.py
@@ -1,13 +1,12 @@
 from machine import Pin, I2C
-from lsm9dso import LSM9DS0
+from lsm9ds0 import LSM9DS0
 import time
 from averager_solution import Averager
 
-i2c = I2C(id=1, sda=Pin(14), scl=Pin(15), freq=3_400_000)
+i2c = I2C(id=1, sda=Pin(18), scl=Pin(19), freq=3_400_000)
 lsm = LSM9DS0(i2c = i2c, a_sens=16)
 
 samples = 10
-
 
 av_x = Averager(samples)
 av_y = Averager(samples)
@@ -16,13 +15,14 @@ av_z = Averager(samples)
 filea = open("test_accel.txt", "w")
 
 while True:
-    time.sleep(10)
     x, y, z = lsm.accel.all()
 
     av_x.append(x)
     av_y.append(y)
     av_z.append(z)
 
-    #filea.write('{} {} {}\n'.format(av_x.get(),av_y.get(),av_z.get()) )
+    filea.write('{} {} {}\n'.format(av_x.get(),av_y.get(),av_z.get()) )
+    time.sleep(0.01)
+    
     
     print(f'x:{av_x.get(): .5f}')
